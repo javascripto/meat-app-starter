b12b941591eba9da980efb43321d5d5775d776b9
Yuri Alves <yurialves2@gmail.com>
2018-11-19 02:28:41 -0200

    Criando barra de pesquisa com animação alternando estado de visibilidade

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantService } from './restaurant.service';
 import { Restaurant } from './restaurant/restaurant.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
   // providers: [RestaurantService], // Serviço disponivel para componente e filhos
   // viewProviders: [RestaurantService], // Serviço disponivel apenas no componente
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out')) // wildcard de trasição de estados
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[];
+  public searchBarState = 'hidden';
+  public restaurants: Restaurant[];
 
   constructor(private restaurantService: RestaurantService) {}
 
@@ -18,6 +34,10 @@ export class RestaurantsComponent implements OnInit {
     this.restaurantService.restaurants().subscribe(restaurants => {
       this.restaurants = restaurants;
     });
+  }
+
+  public toggleSearch(): void {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
